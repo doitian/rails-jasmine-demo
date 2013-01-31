2f8b3ec37c6fdd01791f273e97d89b8623b61d80
Ian Yang <me@iany.me>
2013-01-31 23:15:55 +0800

    rails new rails-jasmine-demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJasmineDemo::Application.config.secret_token = '0e7528a66eb6c4cbb5f40e2f40c531cda257f5a808bd44f302d44f30d8989abf988188ce8f85c727d0f46f1daf8bd2c0331dab864d82b25ee3b54fad24eef876'
